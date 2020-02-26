fb117aae8931fef168101c227c71955f1907fd6b
Justas Grekavicius <justas.grekavicius@gmail.com>
2020-02-26 23:36:54 +0200

    Removed singleton. Refactored Dictionary methods

--- StringAnalyzer/Program.cs
+++ StringAnalyzer/Program.cs
@@ -4,151 +4,123 @@ using System.Linq;
 
 namespace StringAnalyzer
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			StringAnalyzer.getInstance();
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnalyzerDemo.Analyze();
+        }
+    }
 
-	class StringAnalyzer
-	{
-		private static StringAnalyzer _instance = new StringAnalyzer();
+    class AnalyzerDemo
+    {
+        public static string _text { get; private set; }
+        public static Dictionary<string, int> WordFrequencyCount { get; private set; }
+        public static Dictionary<int, int> WordLengthFrequencyCount { get; private set; }
 
-		private string _text { get; set; }
-		private Dictionary<string, int> WordFrequencyCount { get; set; }
-		private Dictionary<int, int> WordLengthFrequencyCount { get; set; }
+        public static void Analyze()
+        {
+            _text = GetInputString();
+            WordFrequencyCount = GetWordsFrequencyCount(_text);
+            WordLengthFrequencyCount = GetWordsLengthFrequencyCount(_text);
+            PrintInfo();
+            Console.ReadLine();
+        }
 
-		private StringAnalyzer() 
-		{
-			AnalyzeString();
-		}
+        private static string GetInputString()
+        {
+            Console.Write("Please input the string you wish to analyze: ");
+            string text = Console.ReadLine();
 
-		public static StringAnalyzer getInstance()
-		{
-			return _instance;
-		}
-		
-		public void AnalyzeString()
-		{
-			_text = GetInputString();
-			WordFrequencyCount = GetWordsFrequencyCount(_text);
-			WordLengthFrequencyCount = GetWordsLengthFrequencyCount(_text);
-			PrintInfo();
-			Console.ReadLine();
-		}
+            if (text.Length == 0)
+            {
+                GetInputString();
+            }
 
-		private string GetInputString()
-		{
-			Console.Write("Please input the string you wish to analyze: ");
-			string text = Console.ReadLine();
+            return text;
+        }
 
-			if (text.Length == 0)
-			{
-				GetInputString();
-			}
-			
-			return text;
-		}
+        private static Dictionary<string, int> GetWordsFrequencyCount(string text)
+        {
+            Dictionary<string, int> results = new Dictionary<string, int>();
 
-		private Dictionary<string, int> GetWordsFrequencyCount(string text)
-		{
-			Dictionary<string, int> results = new Dictionary<string, int>();
+            string[] words = text.Split(' ');
 
-			string[] words = text.Split(' ');
-			int initialCount = 1;
+            foreach (var word in words)
+            {
+                PopulateDictionaryByKey(results, word);
+            }
 
-			try
-			{
-				foreach (var word in words)
-				{
-					if (results.ContainsKey(word))
-					{
-						results[word]++;
-					}
-					else
-					{
-						results.Add(word, initialCount);
-					}
-				}
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine($"Error message: {e.Message}");
-			}
+            return results;
+        }
 
-			return results;
-		}
+        private static Dictionary<int, int> GetWordsLengthFrequencyCount(string text)
+        {
+            Dictionary<int, int> results = new Dictionary<int, int>();
 
-		private Dictionary<int, int> GetWordsLengthFrequencyCount(string text)
-		{
-			Dictionary<int, int> results = new Dictionary<int, int>();
+            string[] words = text.Split(' ');
 
-			string[] words = text.Split(' ');
-			int initialCount = 1;
+            foreach (var word in words)
+            {
+                int wordLength = word.Length;
 
-			try
-			{
-				foreach (var word in words)
-				{
-					int wordLength = word.Length;
+                PopulateDictionaryByKey(results, wordLength);
+            }
 
-					if (results.ContainsKey(wordLength))
-					{
-						results[wordLength]++;
-					}
-					else
-					{
-						results.Add(wordLength, initialCount);
-					}
-				}
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine($"Error message: {e.Message}");
-			}
+            return results;
+        }
 
-			return results;
-		}
+        private static void PopulateDictionaryByKey<T>(IDictionary<T, int> dictionary, T key)
+        {
+            int initialCount = 1;
 
-		private void PrintInfo()
-		{
-			PrintDictionary(WordFrequencyCount);
-			PrintDictionary(WordLengthFrequencyCount);
-		}
 
-		private void PrintDictionary<T>(IDictionary<T, int> dictionary)
-		{
-			string text = "";
-			Type keyType = GetDictionaryKeyType(dictionary);
+            if (dictionary.ContainsKey(key))
+            {
+                dictionary[key]++;
+            }
+            else
+            {
+                dictionary.Add(key, initialCount);
+            }
+        }
 
-			foreach (var record in dictionary)
-			{
-				switch (keyType.Name)
-				{
-					case "String":
-						text += $"\"{record.Key}\": ";
-						break;
-					case "Int32":
-						text += $"{record.Key}: ";
-						break;
-					default:
-						break;
-				}
-				text += $"{record.Value}, ";
-			}
+        private static void PrintInfo()
+        {
+            PrintDictionary(WordFrequencyCount);
+            PrintDictionary(WordLengthFrequencyCount);
+        }
 
-			text = text.Remove(text.Length - 2);
-			Console.WriteLine(text);
-		}
+        private static void PrintDictionary<T>(IDictionary<T, int> dictionary)
+        {
+            string text = "";
+            Type keyType = GetDictionaryKeyType(dictionary);
+                foreach (var record in dictionary)
+                {
+                    switch (keyType.Name)
+                    {
+                        case "String":
+                            text += $"\"{record.Key}\": ";
+                            break;
+                        case "Int32":
+                            text += $"{record.Key}: ";
+                            break;
+                        default:
+                            break;
+                    }
+                    text += $"{record.Value}, ";
+                }
 
-		private Type GetDictionaryKeyType<T> (IDictionary<T, int> dictionary)
-		{
-			Type dictionaryType = dictionary.GetType();
-			Type type = dictionaryType.GetGenericArguments()[0];
+            text = text.Remove(text.Length - 2);
+            Console.WriteLine(text);
+        }
 
-			return type;
-		}
-	}
+        private static Type GetDictionaryKeyType<T>(IDictionary<T, int> dictionary)
+        {
+            Type dictionaryType = dictionary.GetType();
+            Type type = dictionaryType.GetGenericArguments()[0];
+
+            return type;
+        }
+    }
 }
